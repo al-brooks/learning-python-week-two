51ecb51e934431d131cf522db2ca953749190bfe
Alexander Brooks <alexanderlawbrooks@gmail.com>
2021-07-14 15:38:52 -0400

    added setUp function

--- day3_assignment/unit_testing.py
+++ day3_assignment/unit_testing.py
@@ -1,8 +1,10 @@
 import unittest # importing unit test framework
 
+from classes import PoolTable
+
 class PoolTableAppTests(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.pool_table = PoolTable(1)
 
 unittest.main() # run tests
